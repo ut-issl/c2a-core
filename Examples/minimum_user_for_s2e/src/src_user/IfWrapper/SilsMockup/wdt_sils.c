539e8924303f85961f94268d9fafe0cc4004068d
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2021-11-23 01:58:20 +0900

    add Sils mockup for c89 build

--- Examples/minimum_user_for_s2e/src/src_user/IfWrapper/SilsMockup/wdt_sils.c
+++ Examples/minimum_user_for_s2e/src/src_user/IfWrapper/SilsMockup/wdt_sils.c
@@ -2,32 +2,31 @@
 
 int WDT_initialize(void* wdt_config)
 {
-  WDT_Config* my_wdt = (WDT_Config*)wdt_config;
-  WDT_set_timer(wdt_config, 7);
-  my_wdt->is_clear_enable = 1;
-  my_wdt->is_wdt_enable = 1;
-
+  (void)wdt_config;
   return 0;
 }
 
 int WDT_clear(void* wdt_config)
 {
+  (void)wdt_config;
   return 0;
 }
 
 int WDT_enable(void* wdt_config)
 {
+  (void)wdt_config;
   return 0;
 }
 
 int WDT_disable(void* wdt_config)
 {
+  (void)wdt_config;
   return 0;
 }
 
 int WDT_set_timer(void* wdt_config, int time)
 {
-  WDT_Config* my_wdt = (WDT_Config*)wdt_config;
-  my_wdt->timer_setting = time;
+  (void)wdt_config;
+  (void)time;
   return 0;
 }
