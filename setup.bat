fab6ae5c73d523b8c4fafc00653cfc7b1cf22ef1
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2021-11-23 11:53:37 +0900

    change tab to space

--- setup.bat
+++ setup.bat
@@ -8,7 +8,7 @@ echo done.
 echo.
 echo [SET UP DEVELOP ENVIRONMENT FOR MAXIMUM_USER_SAMPLE_FOR_S2E_BUILD]
 if exist ".\Examples\minimum_user_for_s2e\src\src_core" (
-	rmdir /s /q ".\Examples\minimum_user_for_s2e\src\src_core"
+  rmdir /s /q ".\Examples\minimum_user_for_s2e\src\src_core"
 )
 mklink /j /d ".\Examples\minimum_user_for_s2e\src\src_core" .\
 echo done.
