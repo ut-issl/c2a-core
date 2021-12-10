18f37693f27e970b9941cfcb7a073a0520cbb8d5
sksat <sksat@arkedgespace.com>
2021-12-10 22:05:32 +0900

    Fix include path

--- Examples/minimum_user/src/src_user/TlmCmd/telemetry_source.h
+++ Examples/minimum_user/src/src_user/TlmCmd/telemetry_source.h
@@ -6,6 +6,7 @@
 #define TELEMETRY_SOURCE_H_
 
 #include "string.h" // for memcpy
+#include <src_core/git_revision.h>
 #include <src_core/System/ApplicationManager/app_manager.h>
 #include <src_core/System/TaskManager/task_dispatcher.h>
 #include <src_core/System/ModeManager/mode_manager.h>
@@ -15,7 +16,6 @@
 #include <src_core/TlmCmd/command_analyze.h>
 #include <src_core/TlmCmd/block_command_executor.h>
 #include "../Applications/app_headers.h"
-#include "../Library/git_revision.h"
 #include "../Drivers/Aocs/aobc_telemetry_buffer.h"
 #include "../Drivers/Com/gs_validate.h"
 #include "../Settings/build_settings.h"
