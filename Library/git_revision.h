696764693d12eb05222a2ea33d3278ee506374d4
sksat <sksat@arkedgespace.com>
2021-12-13 19:25:46 +0900

    add generate revision header script and option

--- Library/git_revision.h
+++ Library/git_revision.h
@@ -7,15 +7,24 @@
 
 #include <stdint.h>
 
-#ifndef GIT_REVISION_C2A_CORE
-  #define GIT_REVISION_C2A_CORE         "0000000000000000000000000000000000000000"
-  #define GIT_REVISION_C2A_CORE_SHORT   0x0000000
-#endif
+#include <src_user/Library/git_revision.h> // config
 
-#ifndef GIT_REVISION_C2A_USER
-  #define GIT_REVISION_C2A_USER         "0000000000000000000000000000000000000000"
-  #define GIT_REVISION_C2A_USER_SHORT   0x0000000
-#endif
+#ifdef C2A_GIT_REVISION_IGNORE
+// commit hashを埋め込まない場合
+
+#define GIT_REVISION_C2A_CORE         "0000000000000000000000000000000000000000"
+#define GIT_REVISION_C2A_CORE_SHORT   0x0000000
+#define GIT_REVISION_C2A_USER         "0000000000000000000000000000000000000000"
+#define GIT_REVISION_C2A_USER_SHORT   0x0000000
+
+#else
+
+// commit hashを埋め込むはずなのにsrc_user/Library/git_revision.hで設定されていない
+#if !defined(C2A_GIT_REVISION_FROM_OPTION) && !defined(C2A_GIT_REVISION_GENERATED)
+#error please specify git revision config in src_user/Library/git_revision.h
+#endif // error
+
+#endif // C2A_GIT_REVISION_IGNORE
 
 extern const char GIT_REV_CORE[41];
 extern const uint32_t GIT_REV_CORE_SHORT;
