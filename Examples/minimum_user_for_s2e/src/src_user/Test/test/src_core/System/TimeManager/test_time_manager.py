060fddc1ce46c761bb642e7745efbb971e8521ce
chutaro <35984595+chutaro@users.noreply.github.com>
2022-01-14 00:20:00 +0900

    add check of utl_cmd to test_tmgr_set_unixtime

--- Examples/minimum_user_for_s2e/src/src_user/Test/test/src_core/System/TimeManager/test_time_manager.py
+++ Examples/minimum_user_for_s2e/src/src_user/Test/test/src_core/System/TimeManager/test_time_manager.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import time
+import random
 
 import isslwings as wings
 import pytest
@@ -51,8 +52,8 @@ def test_tmgr_set_time():
 @pytest.mark.real
 def test_tmgr_set_unixtime():
 
+    #=====unixtime_at_ti0 が正しく更新できるか確認=====
     current_unixtime = time.time()
-
     ti = 1000
     step = 60
     ret = wings.util.send_cmd_and_confirm(
@@ -63,13 +64,55 @@ def test_tmgr_set_unixtime():
     )
     assert ret == "SUC"
 
-    # 0.05秒の精度でunixtimeが正確に登録されているか確認
     tlm_HK = wings.util.generate_and_receive_tlm(
         ope, c2a_enum.Cmd_CODE_GENERATE_TLM, c2a_enum.Tlm_CODE_HK
     )
     unixtime_at_ti0 = current_unixtime - (ti / 10) - (step / 1000)
-    assert tlm_HK["HK.OBC_TM_UNIXTIME_AT_TI0"] > unixtime_at_ti0 - 0.05
-    assert tlm_HK["HK.OBC_TM_UNIXTIME_AT_TI0"] < unixtime_at_ti0 + 0.05
+    assert tlm_HK["HK.OBC_TM_UNIXTIME_AT_TI0"] ==  unixtime_at_ti0
+
+
+    #=====UTL_cmd が正しく打てるか確認=====
+    # 最初にTL0をクリアしておく
+    ret = wings.util.send_cmd_and_confirm(
+        ope,
+        c2a_enum.Cmd_CODE_TLCD_CLEAR_ALL_TIMELINE,
+        (0, ),
+        c2a_enum.Tlm_CODE_HK,
+    )
+    assert ret == "SUC"
+
+    # NOP を10個, 未来のランダムな時刻で登録する
+    # TODO: wingsがutl_cmdの時刻引数を0.1秒刻みで受け付けるように改修されたら, tiの10の倍数縛りをなくす
+    ti_60_sec_after = (tlm_HK["HK.SH.TI"] // 10) * 10 + 600
+    ti_of_cmds = [ti_60_sec_after + random.randrange(100) * 10 for i in range(10)]
+    unixtime_at_ti0 = (int)(unixtime_at_ti0)
+    for ti in ti_of_cmds:
+        ret = wings.util.send_utl_cmd(
+            ope, unixtime_at_ti0 + ti//10, c2a_enum.Cmd_CODE_NOP, (),
+        )
+    # 重複を削除して時刻順に並べ替え
+    ti_of_cmds = list(set(ti_of_cmds))
+    ti_of_cmds.sort()
+    time.sleep(2)
+
+    # TL0 に正しく登録されているか確認
+    tlm_TL = wings.util.generate_and_receive_tlm(
+        ope, c2a_enum.Cmd_CODE_GENERATE_TLM, c2a_enum.Tlm_CODE_TL
+    )
+    assert tlm_TL["TL.LINE_NO"] == 0
+    for i, ti in enumerate(ti_of_cmds):
+        tlm_name = "TL.CMD" + str(i) + "_TI"
+        assert tlm_TL[tlm_name] <= ti # FIXME: 0.1秒刻みになったら修正する
+        assert tlm_TL[tlm_name] > ti - 10
+
+    # 最後にTL0をもう一度クリアする
+    ret = wings.util.send_cmd_and_confirm(
+        ope,
+        c2a_enum.Cmd_CODE_TLCD_CLEAR_ALL_TIMELINE,
+        (0, ),
+        c2a_enum.Tlm_CODE_HK,
+    )
+    assert ret == "SUC"
 
 
 @pytest.mark.sils
@@ -102,6 +145,6 @@ def test_tmgr_set_utl_unixtime_epoch():
 
 if __name__ == "__main__":
     # test_tmgr_set_time()
-    # test_tmgr_set_unixtime()
+    test_tmgr_set_unixtime()
     # test_tmgr_set_utl_unixtime_epoch()
     pass
