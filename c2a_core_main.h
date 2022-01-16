9712579fe857926bf8bb9a2971461c38915c7f0c
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2022-01-17 03:07:38 +0900

    add version define

--- c2a_core_main.h
+++ c2a_core_main.h
@@ -4,4 +4,11 @@
 void C2A_core_init(void);
 void C2A_core_main(void);
 
+// C2A の バージョンは Semantic Versioning を採用する
+// これらの番号は，リリース時に手動で合わせる
+// 詳細: Docs/General/release.md
+#define C2A_CORE_VER_MAJOR 3
+#define C2A_CORE_VER_MINOR 4
+#define C2A_CORE_VER_PATCH 0
+
 #endif
