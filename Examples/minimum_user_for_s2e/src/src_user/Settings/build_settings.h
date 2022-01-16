6f4f267fb19665dc8ece810ef39fd17858d6e076
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2022-01-16 17:20:36 +0900

    Update build_settings.h

--- Examples/minimum_user_for_s2e/src/src_user/Settings/build_settings.h
+++ Examples/minimum_user_for_s2e/src/src_user/Settings/build_settings.h
@@ -3,8 +3,10 @@
 
 #include "sils_define.h"
 
-#ifndef SILS_FW
-#define IS_LITTLE_ENDIAN // マイコン環境により変更する
+#ifdef SILS_FW
+#define IS_LITTLE_ENDIAN // SILS環境により変更する
+#else
+// #define IS_LITTLE_ENDIAN // マイコン環境により変更する
 #endif
 
 // #define BUILD_SETTINGS_FAST_BUILD    //!< 高速ビルドオプション（検証時は必ずコメントアウトすること）
