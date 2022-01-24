6b3a089c647048d02a4c3f10e095f244135605a0
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2022-01-24 22:04:05 +0900

    add temporary CTCP def

--- Examples/minimum_user_for_s2e/src/src_user/Settings/TlmCmd/common_cmd_packet_define.h
+++ Examples/minimum_user_for_s2e/src/src_user/Settings/TlmCmd/common_cmd_packet_define.h
@@ -10,6 +10,8 @@
 // CommonCmdPacket として TCP をつかう
 // FIXME: CmdSpacePacket に差し替える
 typedef TCP CommonCmdPacket;
+// FIXME: 一時的 https://github.com/ut-issl/c2a-core/pull/209
+typedef TCP CTCP;
 
 // 2021-01-22 追加
 // DEST_TYPE を定義
