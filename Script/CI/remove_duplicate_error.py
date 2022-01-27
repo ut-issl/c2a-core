ce99fb4e2539c9e118f1b2011b4aba571b0fff68
sksat <sksat@arkedgespace.com>
2022-01-27 22:27:04 +0900

    remove duplicate error on reviewdog

---
+++ Script/CI/remove_duplicate_error.py
@@ -0,0 +1,21 @@
+import sys
+
+file = sys.argv[1]
+compiler = sys.argv[2]
+
+with open(file) as f:
+    s = f.read()
+    log = s.split(compiler)
+    if "" in log:
+        log.remove("")
+    # print(log)
+
+    errors = []
+    for l in log:
+        (cmd, err) = l.split("\n", 1)
+        if err in errors:
+            print("duplicate: " + cmd, file=sys.stderr)
+            pass
+        else:
+            errors.append(err)
+            print(compiler + l, end="")
