b562f801539bfffbc3cf871ba3fe5d05ef3626a5
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2022-02-03 03:05:10 +0900

    fix PH small

--- TlmCmd/packet_handler.h
+++ TlmCmd/packet_handler.h
@@ -1,3 +1,7 @@
+/**
+ * @file
+ * @brief C2A 全体を流れる Common Packet の配送を制御する
+ */
 #ifndef PACKET_HANDLER_H_
 #define PACKET_HANDLER_H_
 
@@ -6,8 +10,7 @@
 
 #define PH_GS_CMD_LIST_MAX  (8)
 #define PH_RT_CMD_LIST_MAX  (32)
-#define PH_TL0_CMD_LIST_MAX (TL_TLM_PAGE_SIZE * TL_TLM_PAGE_MAX)   // コメント追加（2019/08/19）
-                                                                   // これが最長じゃないといけない
+#define PH_TL0_CMD_LIST_MAX (TL_TLM_PAGE_SIZE * TL_TLM_PAGE_MAX)   // これが最長じゃないといけない
                                                                    // TLCD_tl_list_for_tlmの長さがこれなので！！
 #define PH_TL1_CMD_LIST_MAX (TL_TLM_PAGE_SIZE * 4)
 #define PH_TL2_CMD_LIST_MAX (TL_TLM_PAGE_SIZE * 4)
@@ -55,18 +58,50 @@ typedef enum
 extern PacketList PH_gs_cmd_list;
 extern PacketList PH_rt_cmd_list;
 extern PacketList PH_tl_cmd_list[TL_ID_MAX];
-// extern PacketList PH_hk_tlm_list;    // EQUではMS TLMに統合
+// extern PacketList PH_hk_tlm_list;    // 現在は MS TLM に統合されている（ TODO: 今後また分離させても良いかも．要検討）
 extern PacketList PH_ms_tlm_list;
 extern PacketList PH_st_tlm_list;
 extern PacketList PH_rp_tlm_list;
 
+/**
+ * @brief Packet Handler を初期化
+ * @param  void
+ * @return void
+ */
 void PH_init(void);
 
-// FIXME: CTCP大改修がおわったらコメント入れる！
+/**
+ * @brief  CTCP を解析する
+ * @note   パケット解析とは，パケットを適切なキューにエンキューすることである（実行することではない）
+ * @note   Common Packet とは， GS から送られてくるものだけでなく，
+ *         C2A 内部で発行されるものや，他の OBC から送られてくるものなどもある
+ * @note   Cmd か Tlm かが確定している場合， PH_analyze_tlm_packet, PH_analyze_cmd_packet を用いる
+ * @param  packet: 解析するパケット
+ * @return PH_ACK
+ */
 PH_ACK PH_analyze_packet(const CommonTlmCmdPacket* packet);
+
+/**
+ * @brief  CTP を解析する
+ * @note   パケット解析とは，パケットを適切なキューにエンキューすることである（実行することではない）
+ * @param  packet: 解析するパケット
+ * @return PH_ACK
+ */
 PH_ACK PH_analyze_tlm_packet(const CommonTlmPacket* packet);
+
+/**
+ * @brief  CCP を解析する
+ * @note   パケット解析とは，パケットを適切なキューにエンキューすることである（実行することではない）
+ * @param  packet: 解析するパケット
+ * @return PH_ACK
+ */
 PH_ACK PH_analyze_cmd_packet(const CommonCmdPacket* packet);
 
+/**
+ * @brief  CCP をコマンドとして解釈して実行，ないしは別機器へ配送する
+ * @param  packet: 実行 or 配送するコマンドパケット
+ * @return CCP_EXEC_STS
+ */
 CCP_EXEC_STS PH_dispatch_command(const CommonCmdPacket* packet);
 
 #endif
