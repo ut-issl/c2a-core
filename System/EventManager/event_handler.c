203f5226e72a62aed0e32a519322421f65ee7faa
Yoshinari Gyu <50069930+yngyu@users.noreply.github.com>
2022-02-27 16:45:39 +0900

    modify logic

--- System/EventManager/event_handler.c
+++ System/EventManager/event_handler.c
@@ -1150,14 +1150,12 @@ EH_CHECK_RULE_ACK EH_inactivate_rule_for_multi_level(EH_RULE_ID id)
   return EH_CHECK_RULE_ACK_OK;
 }
 
-EH_CHECK_RULE_ACK EH_get_rule_is_active(EH_RULE_ID id, uint8_t* is_active)
+uint8_t EH_get_rule_is_active(EH_RULE_ID id)
 {
   EH_CHECK_RULE_ACK ack = EH_check_rule_id_(id);
-  if (ack != EH_CHECK_RULE_ACK_OK) return ack;
+  if (ack != EH_CHECK_RULE_ACK_OK) return 0;
 
-  *is_active = event_handler_.rule_table.rules[id].settings.is_active;
-
-  return EH_CHECK_RULE_ACK_OK;
+  return event_handler_.rule_table.rules[id].settings.is_active;
 }
 
 EH_CHECK_RULE_ACK EH_set_rule_counter(EH_RULE_ID id, uint16_t counter)
--- System/EventManager/event_handler.h
+++ System/EventManager/event_handler.h
@@ -414,10 +414,10 @@ EH_CHECK_RULE_ACK EH_inactivate_rule_for_multi_level(EH_RULE_ID id);
 /**
  * @brief ルールが有効かどうか取得する
  * @param[in]  id: EH_RULE_ID
- * @param[out] is_active: 有効かどうか
- * @return EH_CHECK_RULE_ACK
+ * @return uint8_t: 有効かどうか
+ * @note ID が無効の場合は in_active = 0 を返す
  */
-EH_CHECK_RULE_ACK EH_get_rule_is_active(EH_RULE_ID id, uint8_t* is_active);
+uint8_t EH_get_rule_is_active(EH_RULE_ID id);
 
 /**
  * @brief  EH_Rule の counter をセット
