4b644b6659d0156804e75d39d773a9ba3dacb2d1
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2022-02-28 10:41:48 +0900

    add el debug app

--- Examples/minimum_user_for_s2e/src/src_user/Applications/UserDefined/debug_apps.h
+++ Examples/minimum_user_for_s2e/src/src_user/Applications/UserDefined/debug_apps.h
@@ -6,6 +6,9 @@
 AppInfo APP_DBG_flush_screen(void);
 AppInfo APP_DBG_print_time_stamp(void);
 AppInfo APP_DBG_print_cmd_status(void);
+// 実行時間の問題から分割している
+AppInfo APP_DBG_print_event_logger0(void);
+AppInfo APP_DBG_print_event_logger1(void);
 AppInfo APP_DBG_print_git_rev(void);
 
 #endif
