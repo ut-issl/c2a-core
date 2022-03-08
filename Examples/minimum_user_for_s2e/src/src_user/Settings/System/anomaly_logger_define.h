8431a3637c30cb42e72f862cf8e3fd82af9bc656
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2022-03-09 04:50:53 +0900

    fix include guard

--- Examples/minimum_user_for_s2e/src/src_user/Settings/System/anomaly_logger_define.h
+++ Examples/minimum_user_for_s2e/src/src_user/Settings/System/anomaly_logger_define.h
@@ -3,8 +3,8 @@
  * @brief AL (と AH) を有効化するかどうかの定義
  * @note  AL, AH は EL, EH の新規実装によって deprecated であり，基本的には使うべきではない
  */
-#ifndef ANOMALY_DEFINE_H_
-#define ANOMALY_DEFINE_H_
+#ifndef ANOMALY_LOGGER_DEFINE_H_
+#define ANOMALY_LOGGER_DEFINE_H_
 
 // ここでは Core 開発のため有効化している
 #define AL_ENABLE
