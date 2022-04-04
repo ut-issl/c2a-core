108e96cfd0a74ca9bf297d1f223abdef959801e6
chutaro <35984595+chutaro@users.noreply.github.com>
2022-04-04 16:30:22 +0900

    fix for coding rule

--- Examples/minimum_user_for_s2e/src/src_user/Settings/Applications/timeline_command_dispatcher_define.h
+++ Examples/minimum_user_for_s2e/src/src_user/Settings/Applications/timeline_command_dispatcher_define.h
@@ -2,8 +2,8 @@
  * @file
  * @brief timeline command dispatcher の各種定義
  */
-#ifndef TLCD_DEFINE_H_
-#define TLCD_DEFINE_H_
+#ifndef TIMELINE_COMMAND_DISPATCHER_DEFINE_H_
+#define TIMELINE_COMMAND_DISPATCHER_DEFINE_H_
 
 // TL_MIS (ミッション用の timeline) を使うかどうか
 #define TL_IS_ENABLE_MISSION_TL
