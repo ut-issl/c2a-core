894223a55ddef8e84dfd522d4897780bdc0c029b
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2022-04-26 13:45:33 +0900

    fix small

--- TlmCmd/common_cmd_packet_util.h
+++ TlmCmd/common_cmd_packet_util.h
@@ -77,27 +77,27 @@ void CCP_convert_rtc_to_tlc(CommonCmdPacket* packet, cycle_t ti);
 
 /**
  * @brief  App 実行コマンドを登録
- * @param[in]     ti: TI
- * @param[in]     id: AR_APP_ID
+ * @param[in] ti: TI
+ * @param[in] id: AR_APP_ID
  * @return PH_ACK
  */
 PH_ACK CCP_register_app_cmd(cycle_t ti, AR_APP_ID id);
 
 /**
  * @brief  Realtime command を登録
- * @param[in]     cmd_id: CMD_CODE
- * @param[in]     param:  パラメタ
- * @param[in]     len:    パラメタ長
+ * @param[in] cmd_id: CMD_CODE
+ * @param[in] param:  パラメタ
+ * @param[in] len:    パラメタ長
  * @return CCP_UTIL_ACK
  */
 PH_ACK CCP_register_rtc(CMD_CODE cmd_id, const uint8_t* param, uint16_t len);
 
 /**
  * @brief  Timeline command を登録
- * @param[in]     ti:     TI
- * @param[in]     cmd_id: CMD_CODE
- * @param[in]     param:  パラメタ
- * @param[in]     len:    パラメタ長
+ * @param[in] ti:     TI
+ * @param[in] cmd_id: CMD_CODE
+ * @param[in] param:  パラメタ
+ * @param[in] len:    パラメタ長
  * @return CCP_UTIL_ACK
  */
 PH_ACK CCP_register_tlc(cycle_t ti, CMD_CODE cmd_id, const uint8_t* param, uint16_t len);
@@ -105,8 +105,8 @@ PH_ACK CCP_register_tlc(cycle_t ti, CMD_CODE cmd_id, const uint8_t* param, uint1
 /**
  * @brief  BC展開 command を登録
  * @note   RTC に登録される
- * @param[in]     tl_no: Timeline no
- * @param[in]     block_no: BC ID
+ * @param[in] tl_no: Timeline no
+ * @param[in] block_no: BC ID
  * @return CCP_UTIL_ACK
  */
 PH_ACK CCP_register_block_deploy_cmd(TLCD_ID tl_no, bct_id_t block_no);
