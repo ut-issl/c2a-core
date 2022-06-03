bb4f98d3aad9a2ea430f8a7c19d52d46bf463acc
sksat <sksat@arkedgespace.com>
2022-06-03 18:55:06 +0900

    add BUILD_C2A_AS_UTF8 option(default ON)

--- common.cmake
+++ common.cmake
@@ -24,7 +24,9 @@ if(MSVC)
   if(BUILD_C2A_AS_CXX)
     target_compile_options(${PROJECT_NAME} PUBLIC "/TP") # Compile C codes as C++
   endif()
-  target_compile_options(${PROJECT_NAME} PUBLIC "/source-charset:utf-8")
+  if(BUILD_C2A_AS_UTF8)
+    target_compile_options(${PROJECT_NAME} PUBLIC "/source-charset:utf-8")
+  endif()
 else()
   # SJIS
   # if (NOT CMAKE_C_COMPILER_ID STREQUAL "Clang")
