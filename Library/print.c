23291ccfa485dbefc0fa455bcce1c790bfc4d663
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2022-06-16 04:15:08 +0900

    fix printf

--- Library/print.c
+++ Library/print.c
@@ -1,13 +1,11 @@
 /**
  * @file
- * @brief  HW依存Print
+ * @brief  HW 依存 Print, つまり ログ情報のフォーマット出力（デバッグ出力）
  */
-
-
 // printはHW依存性が強いため，ヘッダーはcoreにおき，適宜userからオーバーライドする
 // ビルド対象にて選択する
 #include "print.h"
 
-void Printf(char* format, ...)
+void Printf(const char* format, ...)
 {
 }
