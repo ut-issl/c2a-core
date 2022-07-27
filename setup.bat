861d1fe9b1ac3cb857334d0e1ed82d3bc884e440
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2022-07-27 19:49:28 +0900

    update setpu

--- setup.bat
+++ setup.bat
@@ -10,17 +10,14 @@ echo.
 
 echo [SET UP DEVELOP ENVIRONMENT FOR C2A MINIMUM USER SAMPLE]
 call :make_link ".\Examples\minimum_user\src\src_core" ".\"
+echo done.
+echo.
 
 echo [SET UP DEVELOP ENVIRONMENT FOR 2ND OBC USER SAMPLE]
 call :make_link ".\Examples\2nd_obc_user\src\src_core" ".\"
-call :make_link ".\Examples\2nd_obc_user\src\s2e_mockup" ".\Examples\minimum_user\src\s2e_mockup"
-call :make_link ".\Examples\2nd_obc_user\src\src_user\BootLoader" ".\Examples\minimum_user\src\src_user\BootLoader"
-call :make_link ".\Examples\2nd_obc_user\src\src_user\Library" ".\Examples\minimum_user\src\src_user\Library"
-call :make_link ".\Examples\2nd_obc_user\src\src_user\ReProgramming" ".\Examples\minimum_user\src\src_user\ReProgramming"
-call :make_link ".\Examples\2nd_obc_user\src\src_user\Script" ".\Examples\minimum_user\src\src_user\Script"
-
 echo done.
 echo.
+
 echo The process has been completed.
 echo Press any key to exit...
 pause >nul
