1601210a149aa828236da50a379faf79dabf44a9
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2022-09-12 15:43:42 +0900

    update cdis test for new el

--- Examples/minimum_user/src/src_user/Test/test/src_core/TlmCmd/test_command_dispatcher.py
+++ Examples/minimum_user/src/src_user/Test/test/src_core/TlmCmd/test_command_dispatcher.py
@@ -23,7 +23,22 @@ AM_TLM_PAGE_MAX = 4
 @pytest.mark.sils
 @pytest.mark.real
 def test_cdis_exec_err():
+    # === 以下の4つを無効パラメタでコマンド登録する
+    # RT Cmd_CODE_TMGR_UPDATE_UNIXTIME
+    # RT Cmd_CODE_AM_SET_PAGE_FOR_TLM
+    # TL Cmd_CODE_TMGR_UPDATE_UNIXTIME
+    # TL Cmd_CODE_AM_SET_PAGE_FOR_TLM
 
+    check_cdis_exec_err(c2a_enum.Cmd_CODE_TMGR_UPDATE_UNIXTIME, (-10, 0, 0))
+    check_cdis_exec_err(c2a_enum.Cmd_CODE_AM_SET_PAGE_FOR_TLM, (AM_TLM_PAGE_MAX + 100,))
+
+    # === 最後にもう一度初期化
+    assert "SUC" == wings.util.send_rt_cmd_and_confirm(
+        ope, c2a_enum.Cmd_CODE_EL_INIT, (), c2a_enum.Tlm_CODE_HK
+    )
+
+
+def check_cdis_exec_err(cmd_id, params):
     # === ELの初期化
     assert "SUC" == wings.util.send_rt_cmd_and_confirm(
         ope, c2a_enum.Cmd_CODE_EL_INIT, (), c2a_enum.Tlm_CODE_HK
@@ -33,53 +48,41 @@ def test_cdis_exec_err():
     )
     ti_now = tlm_EL["EL.SH.TI"]
 
-    # === 以下の4つを無効パラメタでコマンド登録する
-    # RT Cmd_CODE_TMGR_UPDATE_UNIXTIME
-    # RT Cmd_CODE_AM_SET_PAGE_FOR_TLM
-    # TL Cmd_CODE_TMGR_UPDATE_UNIXTIME
-    # TL Cmd_CODE_AM_SET_PAGE_FOR_TLM
-    assert "PRM" == wings.util.send_rt_cmd_and_confirm(
-        ope, c2a_enum.Cmd_CODE_TMGR_UPDATE_UNIXTIME, (-10, 0, 0), c2a_enum.Tlm_CODE_HK
-    )
-    assert "PRM" == wings.util.send_rt_cmd_and_confirm(
-        ope,
-        c2a_enum.Cmd_CODE_AM_SET_PAGE_FOR_TLM,
-        (AM_TLM_PAGE_MAX + 100,),
-        c2a_enum.Tlm_CODE_HK,
-    )
-    wings.util.send_tl_cmd(ope, ti_now + 50, c2a_enum.Cmd_CODE_TMGR_UPDATE_UNIXTIME, (-10, 0, 0))
-    wings.util.send_tl_cmd(
-        ope, ti_now + 55, c2a_enum.Cmd_CODE_AM_SET_PAGE_FOR_TLM, (AM_TLM_PAGE_MAX + 100,)
-    )
-    time.sleep(5)
+    # === 引数が不正な コマンドチェック
+    assert "PRM" == wings.util.send_rt_cmd_and_confirm(ope, cmd_id, params, c2a_enum.Tlm_CODE_HK)
+    wings.util.send_tl_cmd(ope, ti_now + 30, cmd_id, params)
+    time.sleep(3)
 
     # === ELのチェック
     tlm_EL = wings.util.generate_and_receive_tlm(
         ope, c2a_enum.Cmd_CODE_GENERATE_TLM, c2a_enum.Tlm_CODE_EL
     )
+
+    # === ELのチェック
+    tlm_EL = wings.util.generate_and_receive_tlm(
+        ope, c2a_enum.Cmd_CODE_GENERATE_TLM, c2a_enum.Tlm_CODE_EL
+    )
+
     # GS_cmd_dispatcher
-    assert tlm_EL["EL.TLOGS.LOW.EVENTS3.GROUP"] == c2a_enum.EL_CORE_GROUP_CDIS_EXEC_ERR_STS
+    assert tlm_EL["EL.TLOGS.LOW.EVENTS3.GROUP"] == c2a_enum.EL_CORE_GROUP_CDIS_EXEC_ERR_CODE
+    assert tlm_EL["EL.TLOGS.LOW.EVENTS3.NOTE"] == 0
     assert tlm_EL["EL.TLOGS.LOW.EVENTS2.GROUP"] == c2a_enum.EL_CORE_GROUP_CDIS_EXEC_ERR_STS
+    assert (
+        tlm_EL["EL.TLOGS.LOW.EVENTS2.NOTE"] == (cmd_id << 16) | c2a_enum.CCP_EXEC_ILLEGAL_PARAMETER
+    )
     assert tlm_EL["EL.TLOGS.LOW.EVENTS3.LOCAL"] == tlm_EL["EL.TLOGS.LOW.EVENTS2.LOCAL"]
-    note = (c2a_enum.Cmd_CODE_TMGR_UPDATE_UNIXTIME << 16) | c2a_enum.CCP_EXEC_ILLEGAL_PARAMETER
-    assert tlm_EL["EL.TLOGS.LOW.EVENTS3.NOTE"] == note
-    note = (c2a_enum.Cmd_CODE_AM_SET_PAGE_FOR_TLM << 16) | c2a_enum.CCP_EXEC_ILLEGAL_PARAMETER
-    assert tlm_EL["EL.TLOGS.LOW.EVENTS2.NOTE"] == note
+
     # TL_cmd_dispatcher
-    assert tlm_EL["EL.TLOGS.LOW.EVENTS1.GROUP"] == c2a_enum.EL_CORE_GROUP_CDIS_EXEC_ERR_STS
+    assert tlm_EL["EL.TLOGS.LOW.EVENTS1.GROUP"] == c2a_enum.EL_CORE_GROUP_CDIS_EXEC_ERR_CODE
+    assert tlm_EL["EL.TLOGS.LOW.EVENTS1.NOTE"] == 0
     assert tlm_EL["EL.TLOGS.LOW.EVENTS0.GROUP"] == c2a_enum.EL_CORE_GROUP_CDIS_EXEC_ERR_STS
+    assert (
+        tlm_EL["EL.TLOGS.LOW.EVENTS0.NOTE"] == (cmd_id << 16) | c2a_enum.CCP_EXEC_ILLEGAL_PARAMETER
+    )
     assert tlm_EL["EL.TLOGS.LOW.EVENTS1.LOCAL"] == tlm_EL["EL.TLOGS.LOW.EVENTS0.LOCAL"]
-    note = (c2a_enum.Cmd_CODE_TMGR_UPDATE_UNIXTIME << 16) | c2a_enum.CCP_EXEC_ILLEGAL_PARAMETER
-    assert tlm_EL["EL.TLOGS.LOW.EVENTS1.NOTE"] == note
-    note = (c2a_enum.Cmd_CODE_AM_SET_PAGE_FOR_TLM << 16) | c2a_enum.CCP_EXEC_ILLEGAL_PARAMETER
-    assert tlm_EL["EL.TLOGS.LOW.EVENTS0.NOTE"] == note
+
     # GSとTLをlocalで区別できているか
     assert tlm_EL["EL.TLOGS.LOW.EVENTS1.LOCAL"] != tlm_EL["EL.TLOGS.LOW.EVENTS3.LOCAL"]
-
-    # === 最後にもう一度初期化
-    assert "SUC" == wings.util.send_rt_cmd_and_confirm(
-        ope, c2a_enum.Cmd_CODE_EL_INIT, (), c2a_enum.Tlm_CODE_HK
-    )
 
 
 if __name__ == "__main__":
