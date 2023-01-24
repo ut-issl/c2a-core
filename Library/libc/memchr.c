1a439b1cddbbab3de376bbdd12f6bc2e31cde1dd
sksat <sksat@arkedgespace.com>
2023-01-25 06:37:36 +0900

    fix by C2A coding rule 2

--- Library/libc/memchr.c
+++ Library/libc/memchr.c
@@ -2,7 +2,7 @@
 
 // https://linuxjm.osdn.jp/html/LDP_man-pages/man3/memchr.3.html
 
-void* memchr(const void *buf, int c, size_t n)
+void* memchr(const void* buf, int c, size_t n)
 {
   const unsigned char* s = (const unsigned char*) buf;
 
