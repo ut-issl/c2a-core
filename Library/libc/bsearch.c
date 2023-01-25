85149329fba185475073ed8f315e09a5d81e1d39
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2023-01-25 14:18:05 +0900

    fix

--- Library/libc/bsearch.c
+++ Library/libc/bsearch.c
@@ -1,6 +1,6 @@
 /**
  * @file
- * @brief C2A が依存するいくつかの libc 関数 bsearch を自前実装し，c2a-core から提供することで，C2A の移植性を高める．
+ * @brief C2A が依存する libc 関数 bsearch を自前実装し，c2a-core から提供することで，C2A の移植性を高める．
  *        これにより，ベアメタル環境でも C2A を libc 無しに（newlib などを持ち出してくることなく）ビルド・動作させることができる．
  * @note  https://github.com/ut-issl/c2a-core/pull/485
  * @note  https://linuxjm.osdn.jp/html/LDP_man-pages/man3/bsearch.3.html
