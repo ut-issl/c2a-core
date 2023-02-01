a0f5274d515b323ec1f7cb7dffc890d45d6826f8
hoshika-mas~ <hoshika-masato@arkedgespace.com>
2023-02-01 14:52:30 +0900

    fix review comment

--- IfWrapper/Common/i2c_common.c
+++ IfWrapper/Common/i2c_common.c
@@ -5,69 +5,74 @@
 * @note  レジスタの読み書き等，ハードウェアに依存しない関数群を定義する
 */
 
-#include "../i2c.h"
+#include "i2c_common.h"
+#include "../../Library/endian.h"
 #include <string.h>
-#include <src_core/Library/endian.h>
 
-static I2C_ERR_CODE I2C_write_bytes(void* my_i2c_v, uint8_t cmd_byte, void* data_v, uint8_t buffer_size);
-static I2C_ERR_CODE I2C_read_bytes(void* my_i2c_v, uint8_t cmd_byte, void* data_v, uint8_t buffer_size);
+static DS_ERR_CODE I2C_write_bytes_(DriverSuper* p_super, uint8_t stream, void* my_i2c_v, uint8_t cmd_byte, void* data_v, uint8_t buffer_size);
+static DS_ERR_CODE I2C_read_bytes_(DriverSuper* p_super, uint8_t stream, void* my_i2c_v, uint8_t cmd_byte, void* data_v, uint8_t buffer_size);
 
-I2C_ERR_CODE I2C_write_byte(void* my_i2c_v, uint8_t cmd_byte, uint8_t data)
+DS_ERR_CODE I2C_write_byte(DriverSuper* p_super, uint8_t stream, void* my_i2c_v, uint8_t cmd_byte, uint8_t data)
 {
-  return I2C_write_bytes(my_i2c_v, cmd_byte, &data, sizeof(data));
+  return I2C_write_bytes_(p_super, stream, my_i2c_v, cmd_byte, &data, sizeof(data));
 }
 
-I2C_ERR_CODE I2C_write_2bytes(void* my_i2c_v, uint8_t cmd_byte, uint16_t data)
+DS_ERR_CODE I2C_write_2bytes(DriverSuper* p_super, uint8_t stream, void* my_i2c_v, uint8_t cmd_byte, uint16_t data)
 {
-  return I2C_write_bytes(my_i2c_v, cmd_byte, &data, sizeof(data));
+  return I2C_write_bytes_(p_super, stream, my_i2c_v, cmd_byte, &data, sizeof(data));
 }
 
-static I2C_ERR_CODE I2C_write_bytes(void* my_i2c_v, uint8_t cmd_byte, void* data_v, uint8_t buffer_size)
+static DS_ERR_CODE I2C_write_bytes_(DriverSuper* p_super, uint8_t stream, void* my_i2c_v, uint8_t cmd_byte, void* data_v, uint8_t buffer_size)
 {
   uint8_t tx_data[sizeof(cmd_byte) + sizeof(uint16_t)];
+  DS_StreamConfig* stream_config = &(p_super->stream_config[stream]);
 
   tx_data[0] = cmd_byte;
   ENDIAN_memcpy(tx_data + sizeof(cmd_byte), data_v, buffer_size);
   I2C_set_stop_flag(my_i2c_v, 1);
-  return (I2C_ERR_CODE)I2C_tx(my_i2c_v, tx_data, sizeof(cmd_byte) + buffer_size);
+  DSSC_set_tx_frame(stream_config, tx_data);
+  DSSC_set_tx_frame_size(stream_config, sizeof(cmd_byte) + buffer_size);
+
+  return DS_send_general_cmd(p_super, stream);
 }
 
-I2C_ERR_CODE I2C_read_byte(void* my_i2c_v, uint8_t cmd_byte, uint8_t* data)
+DS_ERR_CODE I2C_read_byte(DriverSuper* p_super, uint8_t stream, void* my_i2c_v, uint8_t cmd_byte, uint8_t* data)
 {
-  return I2C_read_bytes(my_i2c_v, cmd_byte, data, sizeof(uint8_t));
+  return I2C_read_bytes_(p_super, stream, my_i2c_v, cmd_byte, data, sizeof(uint8_t));
 }
 
-I2C_ERR_CODE I2C_read_2bytes(void* my_i2c_v, uint8_t cmd_byte, uint16_t* data)
+DS_ERR_CODE I2C_read_2bytes(DriverSuper* p_super, uint8_t stream, void* my_i2c_v, uint8_t cmd_byte, uint16_t* data)
 {
-  uint8_t rx_data[sizeof(uint16_t)];
-  I2C_ERR_CODE ret = I2C_OK;
-
-  ret = I2C_read_bytes(my_i2c_v, cmd_byte, rx_data, sizeof(uint16_t));
-  ENDIAN_memcpy(data, rx_data, sizeof(uint16_t));
-
-  return ret;
+  return I2C_read_bytes_(p_super, stream, my_i2c_v, cmd_byte, data, sizeof(uint8_t));
 }
 
-static I2C_ERR_CODE I2C_read_bytes(void* my_i2c_v, uint8_t cmd_byte, void* data_v, uint8_t buffer_size)
+static DS_ERR_CODE I2C_read_bytes_(DriverSuper* p_super, uint8_t stream, void* my_i2c_v, uint8_t cmd_byte, void* data_v, uint8_t buffer_size)
 {
-  I2C_ERR_CODE ret = I2C_OK;
+  DS_ERR_CODE ret = DS_ERR_CODE_OK;
+  const uint8_t* rx_data;
+  DS_StreamConfig* stream_config = &(p_super->stream_config[stream]);
 
   // read setting
+  DSSC_set_rx_frame_size(stream_config, buffer_size);
   I2C_set_rx_length(my_i2c_v, buffer_size);
+  DS_clear_rx_buffer(p_super);
   // send
   I2C_set_stop_flag(my_i2c_v, 0);
-  ret = (I2C_ERR_CODE)I2C_tx(my_i2c_v, &cmd_byte, sizeof(cmd_byte));
-  if (ret != I2C_OK)
-  {
-    return ret;
-  }
+  DSSC_set_tx_frame(stream_config, &cmd_byte);
+  DSSC_set_tx_frame_size(stream_config, sizeof(cmd_byte));
+  ret = DS_send_req_tlm_cmd(p_super, stream);
+  if (ret != DS_ERR_CODE_OK) return ret;
   // read
   I2C_set_stop_flag(my_i2c_v, 1);
-  ret = (I2C_ERR_CODE)I2C_rx(my_i2c_v, data_v, buffer_size);
-  if (ret > 0)
+  ret = DS_receive(p_super);
+  if (ret != DS_ERR_CODE_OK) return ret;
+  if (DSSC_get_rec_status(stream_config)->status_code != DS_STREAM_REC_STATUS_FIXED_FRAME)
   {
-    ret = I2C_OK;
+    return DS_ERR_CODE_ERR;
   }
+
+  rx_data = DSSC_get_rx_frame(stream_config);
+  ENDIAN_memcpy(data_v, rx_data, buffer_size);
 
   return ret;
 }
--- IfWrapper/Common/i2c_common.h
+++ IfWrapper/Common/i2c_common.h
@@ -7,4 +7,51 @@
 #ifndef I2C_COMMON_H_
 #define I2C_COMMON_H_
 
+#include "../i2c.h"
+#include "../../Drivers/Super/driver_super.h"
+
+/**
+ * @brief I2C_Config 構造体にて指定されたchから1バイトデータを書き込みます
+ * @param[in] p_super  : DriverSuper 構造体へのポインタ
+ * @param[in] stream   : 使用する stream_config の番号
+ * @param[in] my_i2c_v : 対象とする I2C_Config 構造体へのポインタ
+ * @param[in] cmd_byte : コマンド
+ * @param[in] data     : 書き込むデータ
+ * @retval DS_ERR_CODE
+ */
+DS_ERR_CODE I2C_write_byte(DriverSuper* p_super, uint8_t stream, void* my_i2c_v, uint8_t cmd_byte, uint8_t data);
+
+/**
+ * @brief I2C_Config 構造体にて指定されたchから2バイトデータを書き込みます
+ * @param[in] p_super  : DriverSuper 構造体へのポインタ
+ * @param[in] stream   : 使用する stream_config の番号
+ * @param[in] my_i2c_v : 対象とする I2C_Config 構造体へのポインタ
+ * @param[in] cmd_byte : コマンド
+ * @param[in] data     : 書き込むデータ
+ * @retval DS_ERR_CODE
+ */
+DS_ERR_CODE I2C_write_2bytes(DriverSuper* p_super, uint8_t stream, void* my_i2c_v, uint8_t cmd_byte, uint16_t data);
+
+/**
+ * @brief I2C_Config 構造体にて指定されたchから1バイトデータを読み込みます
+ * @param[in] p_super  : DriverSuper 構造体へのポインタ
+ * @param[in] stream   : 使用する stream_config の番号
+ * @param[in] my_i2c_v : 対象とする I2C_Config 構造体へのポインタ
+ * @param[in] cmd_byte : コマンド
+ * @param[out] data_v  : データ格納先へのポインタ
+ * @retval DS_ERR_CODE
+ */
+DS_ERR_CODE I2C_read_byte(DriverSuper* p_super, uint8_t stream, void* my_i2c_v, uint8_t cmd_byte, uint8_t* data_v);
+
+/**
+ * @brief I2C_Config 構造体にて指定されたchから2バイトデータを読み込みます
+ * @param[in] p_super  : DriverSuper 構造体へのポインタ
+ * @param[in] stream   : 使用する stream_config の番号
+ * @param[in] my_i2c_v : 対象とする I2C_Config 構造体へのポインタ
+ * @param[in] cmd_byte : コマンド
+ * @param[out] data_v  : データ格納先へのポインタ
+ * @retval DS_ERR_CODE
+ */
+DS_ERR_CODE I2C_read_2bytes(DriverSuper* p_super, uint8_t stream, void* my_i2c_v, uint8_t cmd_byte, uint16_t* data_v);
+
 #endif
