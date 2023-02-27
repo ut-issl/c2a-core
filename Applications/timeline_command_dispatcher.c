a8a6da58d8d49eedef68a0a321ea8e2c85ca5c50
chutaro <35984595+chutaro@users.noreply.github.com>
2023-02-28 04:37:55 +0900

    BC展開時刻被りはコマンド実行エラーにしないようにした

--- Applications/timeline_command_dispatcher.c
+++ Applications/timeline_command_dispatcher.c
@@ -275,8 +275,15 @@ CCP_CmdRet Cmd_TLCD_DEPLOY_BLOCK(const CommonCmdPacket* packet)
     EL_record_event((EL_GROUP)EL_CORE_GROUP_TLCD_DEPLOY_BLOCK,
                     (uint32_t)ack,
                     EL_ERROR_LEVEL_LOW,
-                    (uint32_t)( ((0x000000ff & id) << 24) | (0x00ffffff & block_no) ));
-    return CCP_make_cmd_ret(CCP_EXEC_ILLEGAL_CONTEXT, (uint32_t)ack);
+                    (uint32_t)block_no);
+    if (ack == PL_BC_TIME_ADJUSTED)
+    {
+      return CCP_make_cmd_ret(CCP_EXEC_SUCCESS, (uint32_t)ack);
+    }
+    else
+    {
+      return CCP_make_cmd_ret(CCP_EXEC_ILLEGAL_CONTEXT, (uint32_t)ack);
+    }
   }
 
   return CCP_make_cmd_ret_without_err_code(CCP_EXEC_SUCCESS);
