8bbff53f7a2445c6d1443c52b734784aad4129ae
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2023-03-13 03:14:45 +0900

    del src of AL and AH

--- Examples/minimum_user/src/src_user/Settings/Applications/AnomalyHandlerRules/ahr_default_load.c
+++
@@ -1,9 +0,0 @@
-#pragma section REPRO
-#include <src_core/Applications/anomaly_handler.h>
-// ah_rules.h はここではincludeしない！
-
-void AH_Rule_default_load(void)
-{
-}
-
-#pragma section
--- Examples/minimum_user/src/src_user/Settings/System/anomaly_logger_settings.h
+++
@@ -1,6 +0,0 @@
-#ifndef ANOMALY_LOGGER_SETTINGS_H_
-#define ANOMALY_LOGGER_SETTINGS_H_
-
-void AL_load_default_settings(void);
-
-#endif
