b1bbabbbbe670a3382518aefa1a962da6b9cdc5e
Ryo Suzumoto <ryo.suzumoto@gmail.com>
2023-04-09 22:42:12 +0900

    update version

--- c2a_core_main.h
+++ c2a_core_main.h
@@ -8,8 +8,8 @@ void C2A_core_main(void);
 // これらの番号は，リリース時に手動で合わせる
 // 詳細: Docs/General/release.md
 #define C2A_CORE_VER_MAJOR (3)
-#define C2A_CORE_VER_MINOR (8)
+#define C2A_CORE_VER_MINOR (9)
 #define C2A_CORE_VER_PATCH (0)
-#define C2A_CORE_VER_PRE   ("")
+#define C2A_CORE_VER_PRE   ("beta.0")
 
 #endif
