78637a0e01ccc4f3341c69e9a04018dec806a3e4
sksat <sksat@arkedgespace.com>
2023-05-11 01:58:02 +0900

    add source dir export func in export-src feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "export-src")]
+pub fn source_dir() -> std::path::PathBuf {
+    std::path::Path::new(env!("CARGO_MANIFEST_DIR")).to_path_buf()
+}
